76e4f26ad81a8116afd2900cbb02498a3045c36c
pIuryc <142180218+pIuryc@users.noreply.github.com>
2023-12-13 22:22:59 -0300

    Update ff.h

--- Projeto/ff.h
+++ Projeto/ff.h
@@ -15,7 +15,7 @@ struct pgm
 };
 
 void readPGMImage(struct pgm *, char *);
-void quantizeImage(unsigned char *, int, int, int);
+void quantizeImage(unsigned char *, int, int, int, int);
 void filtraMatriz(unsigned char *, int, int, int);
 int criaCoocorrencias(int *, unsigned char *, unsigned char *, int, int ,int);
 void gerarSCM(unsigned char *, unsigned char *, char *, int, struct pgm *pio, int);
